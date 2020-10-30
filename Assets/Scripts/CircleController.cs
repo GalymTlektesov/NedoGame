7a5aa27964dd694de42eed26b293d24a9758439d
Brodator <brodatorl@example.com>
2020-10-30 14:52:01 +0600

    Upload the juggle game

---
+++ Assets/Scripts/CircleController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CircleController : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    public Text text;
+    int points = 0;
+    public GameObject platform;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.GetComponent<WhitePlatformController>() != null )
+        {
+            points++;
+            rigidbody.velocity = new Vector2(Random.Range(-10.0f, 10.0f), rigidbody.velocity.y);
+            text.text = points.ToString();
+        }
+
+
+        PlatCol("PlatformWhiteSprite (4)", Random.Range(-10.0f, 10.0f), rigidbody.velocity.y, collision);
+        PlatCol("PlatformWhiteSprite (2)", rigidbody.velocity.x, Random.Range(-5.0f, 5.0f), collision);
+        PlatCol("PlatformWhiteSprite (3)", rigidbody.velocity.x, Random.Range(-5.0f, 5.0f), collision);
+        Lose("PlatformWhiteSprite (1)", collision);
+    }
+    private void Lose(string name, Collision2D collision)
+    {
+        if(collision.gameObject.name== name)
+        {
+            Destroy(gameObject);
+            Destroy(platform);
+            text.text = "Вы проебали!";
+        }
+
+    }
+
+    private void PlatCol(string name, float x, float y, Collision2D collision)
+    {
+        if (collision.gameObject.name == name)
+        {
+            rigidbody.velocity = new Vector2(x, y);
+        }
+    }
+}
---
+++ Assets/Scripts/WhitePlatformController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class WhitePlatformController : MonoBehaviour
+{
+    public float maxSpeed = 10f;
+    public Rigidbody2D rigidbody;
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        float moving = Input.GetAxis("Horizontal");
+        rigidbody.velocity = new Vector2(moving * maxSpeed, rigidbody.velocity.y);
+    }
+}
